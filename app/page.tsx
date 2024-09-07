e1472336cfceececac305b81f015983aeec869a2
Billa05 <workinguse5@gmail.com>
2024-09-07 20:32:59 +0530

    added more components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 // import buttonVariants from "@/components/ui/button"
+import Footer from "@/components/Footer";
 import FormComponent from "@/components/Form";
+import Modal from "@/components/Modal";
+import Navbar from "@/components/Navbar";
+import Signin from "@/components/Signin";
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <FormComponent/>
+      <Navbar/>
+      {/* <FormComponent/> */}
+      {/* <Modal/> */}
+      {/* <Signin/> */}
     </div>
   );
 }
